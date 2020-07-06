2f3c24ab3c80298dcd902d721c1ab22b59e0e1b8
Iuri Souza <iuri.santanna@tvglobo.com.br>
2020-07-05 22:22:06 -0300

    fix: account schema

--- modulo4/pratica/src/models/account.js
+++ modulo4/pratica/src/models/account.js
@@ -4,7 +4,7 @@ const schema = new mongoose.Schema({
   agencia: { type: Number, required: true },
   conta: { type: Number, required: true },
   name: { type: String, required: true },
-  balance: { type: Number, min: 18, required: true },
+  balance: { type: Number, min: 0, required: true },
 });
 
 export default schema;
